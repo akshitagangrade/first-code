451fe25f49368d90daebc24e32df6c3b36e4b329
akshita gangrade <akshitagangrade2@gmail.com>
2016-06-09 12:54:51 +0530

    modified assignment.groovy to find age thar are greater than or less than 20

--- src/assignment1.groovy
+++ src/assignment1.groovy
@@ -14,7 +14,7 @@ class User{
      */
 }
 Map m1 = [firstName: "AKshita", lastName: "Gangrade", email: "dus@gmail.com" , age:21]
-Map m2= [firstName: "smita", lastName: "p", email: "csdbsds" , age:20]
+Map m2= [firstName: "smita", lastName: "p", email: "csdbsds" , age:10]
 Map m3 = [firstName: "sagar", lastName: "jain", email: "csdbsds" , age:53]
 Map m4 = [firstName: "priyal", lastName: "sds", email: "csdbsds" , age:20]
 Map m5 = [firstName: "sagar", lastName: "jain", email: "csdbsds" , age:21]
@@ -36,3 +36,28 @@ a.each {
     {
     println it
  */
+
+
+/*to check the age of 5 users and store age into array whose age s less than 20
+and store age into another aray whose age is greater than 20 */
+
+List b = []
+List d = []
+
+a.each {item ->
+    if (item.age > 20) {
+       // println "${item.age}"
+        b.push (item.age)
+        }
+    if ( item.age < 20){
+        d.push(item.age)
+    }
+} println("hen age is regater than 20")
+b.each {
+    println it
+} println "age is greater thn 20"
+d.each {
+    println it
+}
+
+
